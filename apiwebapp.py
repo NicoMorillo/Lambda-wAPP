6e5c3854f975a44ea27528ee88af8f5974d13906
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2021-10-09 00:09:14 +0100

    first commit

---
+++ apiwebapp.py
@@ -0,0 +1,17 @@
+import json
+
+def handler_form(event, contex):
+
+    textEmail
+    textTitle
+    textFirst_name
+    textLast_name
+    textAddress_line1
+    textAddress_line2
+    textCity
+    textPostcode
+    textTelephone
+    textSite
+
+
+    return formData
