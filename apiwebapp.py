cbdd12a18f456646bd6e382fa1f36e7c1781bb50
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2021-10-10 20:19:41 +0100

    Previoys depl

--- apiwebapp.py
+++ apiwebapp.py
@@ -1,17 +1,27 @@
 import json
 
-def handler_form(event, contex):
+def lambda_handler(event, context):
 
-    textEmail
-    textTitle
-    textFirst_name
-    textLast_name
-    textAddress_line1
-    textAddress_line2
-    textCity
-    textPostcode
-    textTelephone
-    textSite
+    textEmail = event['queryStringParameters']['textEmail']
+    textTitle = event['queryStringParameters']['textTitle']
+    textFirst_name = event['queryStringParameters']['textFirst_name']
+    textLast_name = event['queryStringParameters']['textLast_name']
+    textAddress_line1 = event['queryStringParameters']['textAddress_line1']
+    textAddress_line2 = event['queryStringParameters']['textAddress_line2']
+    textCity = event['queryStringParameters']['textCity']
+    textPostcode = event['queryStringParameters']['textPostcode']
+    textTelephone = event['queryStringParameters']['textTelephone']
+    textSite = event['queryStringParameters']['textSite']
+
+    app_response = {}
+    app_response['message'] = "Welcome " + textFirst_name + textLast_name + " thank you for register in this app"
+    app_response['access_log'] = "Your access login is " + textEmail
+
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(app_response)
 
 
-    return formDat
+    return responseObject
