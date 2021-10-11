573feca7b61b2e51bbb4eb957b102917ae2d952b
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2021-10-12 00:12:31 +0100

    gmtime-fix

--- mysql.py
+++ mysql.py
@@ -1,13 +1,13 @@
 import json
 import boto3
-from time import gtime, strftime
+from time import gmtime, strftime
 
 
 dynamodb = boto3.resource('dynamodb')
 
 table = dynamedb.Table('WebAppDB')
 
-now = strftime(" %a,%d %b %Y %H:%M:%S +0000", gtime())
+now = strftime(" %a,%d %b %Y %H:%M:%S +0000", gmtime())
 
 def lambda_handler(event, context):
 
